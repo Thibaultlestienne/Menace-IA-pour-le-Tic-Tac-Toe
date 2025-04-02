19afd1b162424ef1e4f96c64d083801fcc2c3f27
Thibault Lestienne <99330619+Thibaultlestienne@users.noreply.github.com>
2025-04-02 17:48:32 +0200

    mise a jour des sources

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 L'objectif de ce programme est de créer une IA pour jouer au morpion basée sur le système MENACE (cf. sources)
 
 SOURCES :
-    https://images.math.cnrs.fr/Une-machine-en-boites-d-allumettes-qui-apprend-a-jouer-au-Morpion.html
+    https://images.math.cnrs.fr/billets/une-machine-en-boites-dallumettes-qui-apprend-a-jouer-au-morpion/
     https://en.wikipedia.org/wiki/Matchbox_Educable_Noughts_and_Crosses_Engine
 """
 from Morpion import *
